e15685b07539094f63f1e10e1433da63405d7353
Euclid Stellar <euclidstellar@gmail.com>
2023-08-26 02:36:57 +0530

    formmated code and converted kelvin temp to celcius using variable

--- lib/envdata.dart
+++ lib/envdata.dart
@@ -1,1 +1,9 @@
 const weatherapikey = '5a1139a980e31152039d863a204837be';
+
+
+
+// API Key for OpenWeatherMap.org
+
+// af94be930f5f70acc2d1b12cd8d5e88e
+// b67e5c684bd7777f9685d7dccb1559e7
+// 880800201440af9e6b4de3808c0d510c
